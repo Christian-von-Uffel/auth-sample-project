129dff4824be949cceaecb893a32c86feccbbf39
Christian von Uffel <christian.uffel@gmail.com>
2021-09-13 17:04:50 -0500

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NuxtLink to="login" class="bg-blue-400 py-2 px-3 rounded shadow transition duration-150">
+      Login
+    </NuxtLink>
+    <Tutorial/>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
